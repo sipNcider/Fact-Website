4e3986c92cd2607da1ac69213c1c09c849a448be
Isaiah Allen <izayasipi@gmail.com>
2026-02-12 23:29:23 -0700

    0.0.0

---
+++ script.js
@@ -0,0 +1,26 @@
+const box = document.getElementById("fact-box");
+const btn = document.getElementById("button");
+
+btn.addEventListener("click", function () {
+    let randomNumber = Math.floor(Math.random() * 5) + 1;
+    switch (randomNumber) {
+        case 1:
+            box.textContent = "Miku LOOVEEE leek!!!";
+            break;
+        case 2:
+            box.textContent = "Miku is MEXICAN!!";
+            break;
+        case 3:
+            box.textContent = "I love femboys";
+            break;
+        case 4:
+            box.textContent = "Pecans";
+            break;
+        case 5:
+            box.textContent = "Eat more leek!";
+            break;
+        default:
+            box.textContent = "smth wrong twin";
+            break;
+    }
+});
